02a326b88d71f5848aad90a6a72b9d02e13cc791
Young <YoungLet@126.com>
2026-03-17 13:38:26 +0800

    cursor: empty commit to allow worktree creation

---
+++ src/main/java/com/example/pwps/PwpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.pwps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pwps/PwpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pwps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
